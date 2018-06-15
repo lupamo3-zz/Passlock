f301891247e071f42bffb2cc9ea3e34337e8d577
lan <lupamo93@gmail.com>
2018-06-15 15:04:33 +0300

    setup method to test

--- newpasslock.py
+++ newpasslock.py
@@ -13,3 +13,18 @@ class Login:
         self.email=email
         self.password=password
 
+class Pword:
+    """
+    Class that stores password and user ids
+    """
+    pword_list=[] #Empty pword list
+
+    def __init__(self, userid, website, password)
+        '''
+        __init__ method helps us to store user details
+        '''
+        self.userid=userid
+        self.website=website
+        self.password=password
+
+        
---
+++ newpasslock_test.py
@@ -0,0 +1,21 @@
+import unittest #Importing the unittest module
+from newpasslock import Login #Import the login class
+
+class TestLogin(unittest.TestCase):
+    '''
+    Testclass that defines login class behaviours
+    Args: unittest.Testcase class helps in creating test cases.
+    '''
+
+    def setUp(self):
+        '''
+        Setup method to run each test cases.
+        '''
+        self.new_newpasslock=Login("skankhunt42", "picklerick@gmail.com", "123456") #create login object
+
+    def test_init(self):
+        '''
+        test_init test case to test if object is initialized properly
+        '''
+        
+
